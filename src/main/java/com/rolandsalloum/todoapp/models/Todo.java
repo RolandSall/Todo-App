572c562e649393d731dea26801a2c19109e92ad8
Roland Salloum <roland.salloum00@outlook.com>
2021-01-04 19:24:22 +0200

    feat: added postgres db in docker-compose.yml

--- src/main/java/com/rolandsalloum/todoapp/models/Todo.java
+++ src/main/java/com/rolandsalloum/todoapp/models/Todo.java
@@ -2,6 +2,8 @@ package com.rolandsalloum.todoapp.models;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.UUID;
 
 @NoArgsConstructor
@@ -9,7 +11,10 @@ import java.util.UUID;
 @Getter
 @Setter
 @Builder
+@Entity
 public class Todo {
+
+    @Id
     private UUID todoId;
     private String title;
     private String description;
